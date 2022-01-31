2cab8ec1f6900e18c259fda33e2b97aed5c64d7e
Akin <akinkavak1@gmail.com>
2022-01-31 17:03:32 +0300

    Adding useState hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
 function Counter() {
+
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    setCount(previousState => previousState + 1) 
   }
-
   return <button onClick={increment}>I have been clicked {count} times</button>;
 }
 
